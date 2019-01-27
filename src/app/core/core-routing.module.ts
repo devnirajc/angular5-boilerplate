2a62f3ac1af18ba4df3faddbd3dcb100b44c9ddc
nirajcxpanxion <nchavan@xpanxion.com>
2019-01-27 19:57:13 +0530

    file component added

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {
+  LoginComponent,
+  NotFoundComponent,
+  SignupComponent
+} from '@app/core/components/index';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: '@app/modules/layout/layout.module#LayoutModule'
+  }, {
+    path: 'login',
+    component: LoginComponent
+  }, {
+    path: 'signup',
+    component: SignupComponent
+  }, {
+    path: 'not-found',
+    component: NotFoundComponent
+  }, {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class CoreRoutingModule { }
--- src/app/shared/components/file-upload/file-upload.component.ts
+++ src/app/shared/components/file-upload/file-upload.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
+import { Key } from 'protractor';
 
 @Component({
   selector: 'app-file-upload',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./file-upload.component.scss']
 })
 export class FileUploadComponent implements OnInit {
+  @Input() fileDropContainer: string;
+  @Input() fileUploadId: string = '';
+  @Input() disabled: boolean = false;
+  @Input() multiple: boolean = false;
+  @Input() resetFileData: boolean = false;
+
+  @Output() InputChanged = new EventEmitter<any>();
+
+  @ViewChild('file') file = null;
+  public files: Set<File> = new Set();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public onFilesAdded = () => {
+    const files: { [key: string]: File } = this.file.nativeElement.files;
+    this.InputChanged.emit(files);
+  }
+
+  public resetFiles = () => {
+    (this.resetFileData) ? this.file.nativeElement.value = '' : this.onFilesAdded();
   }
 
 }
